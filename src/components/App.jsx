a068c821e96d78e8723a8a1ca8229ac239ba0cdf
Vitaliy2773 <vitalii64773@gmail.com>
2023-11-19 19:06:21 -1100

    task 6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact, setFilter } from './Contacts/contactsSlice';
+import ContactForm from './Contacts/ContactForm';
+import Filter from './Contacts/Filter';
+import ContactList from './Contacts/ContactList';
+import css from './Contacts/Contacts.module.css';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
+
+  function handleSubmit(name, number) {
+    const contactExists = contacts.some(
+      contact =>
+        typeof contact.name === 'string' &&
+        contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (contactExists) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    dispatch(addContact(name, number));
+  }
+
+  function handleDeleteContact(contactId) {
+    dispatch(deleteContact(contactId));
+  }
+
+  function changeFilter(e) {
+    dispatch(setFilter(e.target.value));
+  }
+
+  const filteredContacts = contacts.filter(
+    contact =>
+      typeof contact.name === 'string' &&
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onSubmit={handleSubmit} />
+      <h2 className={css.title}>Contacts</h2>
+      <Filter onChange={changeFilter} value={filter} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteItem={handleDeleteContact}
+      />
     </div>
   );
-};
+}
